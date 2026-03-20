2ed0b2acbe3ebd246298370ea0c61f4be2bdee42
ahmed-x86 <ahmdansmr7@gmail.com>
2026-03-20 07:48:13 +0200

    commint

--- github_radar.py
+++ github_radar.py
@@ -18,8 +18,8 @@ from dotenv import load_dotenv
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 HOME_DIR = os.path.expanduser("~")
-ENV_PATH = os.path.join(HOME_DIR, ".config/waybar/scripts/.env")
-SOUND_PATH = os.path.join(HOME_DIR, ".config/sounds/freesound_community-retro-audio-logo-94648.mp3")
+ENV_PATH = os.path.join(HOME_DIR, ".config/waybar/scripts/.env")       #home dir for any user
+SOUND_PATH = os.path.join(HOME_DIR, ".config/sounds/freesound_community-retro-audio-logo-94648.mp3") #you can change this sound 
 
 load_dotenv(ENV_PATH)
 
