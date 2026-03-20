0715df04b5ecc8df710e999071632f99bb0a5f1c
ahmed-x86 <ahmdansmr7@gmail.com>
2026-03-20 07:51:12 +0200

    commint

--- github_radar.py
+++ github_radar.py
@@ -23,7 +23,7 @@ SOUND_PATH = os.path.join(HOME_DIR, ".config/sounds/freesound_community-retro-au
 
 load_dotenv(ENV_PATH)       #loding .env file
 
-GITHUB_USERNAME = os.getenv("GITHUB_USERNAME")
+GITHUB_USERNAME = os.getenv("GITHUB_USERNAME")      #username in thr .env file
 GITHUB_PAT = os.getenv("GITHUB_PAT")
 
 if not GITHUB_USERNAME or not GITHUB_PAT:
